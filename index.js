dd633dcb47f86a45e939f11f227a5ff8e68975d5
fabwaseem <waseemaofficial@gmail.com>
2023-07-21 22:58:24 +0500

    added contact form handling

--- index.js
+++ index.js
@@ -73,6 +73,44 @@ app.get("/analytics", async (req, res) => {
   }
 });
 
+const contactSchema = mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  email: String,
+  phone: String,
+  message: String,
+});
+
+const Contact = mongoose.model("Contact", contactSchema);
+
+app.post("/contact", async (req, res) => {
+  const { firstName, lastName, email, phone, message } = req.body;
+
+  // Validate that required fields are provided
+  if (!firstName || !lastName || !email || !message) {
+    return res
+      .status(400)
+      .json({ message: "Please fill in all required fields" });
+  }
+
+  const contact = new Contact({
+    firstName,
+    lastName,
+    email,
+    phone,
+    message,
+  });
+
+  try {
+    await contact.save();
+    return res
+      .status(200)
+      .json({ message: "Contact form data saved successfully" });
+  } catch (error) {
+    return res.status(500).json({ message: "Something went wrong" });
+  }
+});
+
 app.get("/", (req, res) => {
   res.send("Hello to picExpert API");
 });
